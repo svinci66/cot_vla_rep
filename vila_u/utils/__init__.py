a47cbce9c64ffddce85cc88fc02fc36b09e93f09
索键 <sauvinci@svMacBook-Air.local>
2026-04-07 15:51:45 +0800

    Fix utils module imports: restore original wildcard import
    - Restore `from .utils import *` to export make_list and other utility functions
    - Keep LiberoSaver exports for trajectory saving functionality
    - Fixes ImportError in vila_u/utils/media.py that requires make_list

--- vila_u/utils/__init__.py
+++ vila_u/utils/__init__.py
@@ -1,7 +1,2 @@
-"""
-VILA-U utilities module
-"""
-
+from .utils import *
 from .libero_saver import LiberoSaver, verify_libero_format, convert_trajectory_to_libero
-
-__all__ = ['LiberoSaver', 'verify_libero_format', 'convert_trajectory_to_libero']
