2c48152a49c3a9b657b99a3039451cedd4c601ca
索键 <sauvinci@svMacBook-Air.local>
2026-04-09 14:49:40 +0800

    Add logging fallback when loguru is missing

--- vila_u/utils/logging.py
+++ vila_u/utils/logging.py
@@ -1,17 +1,30 @@
+import logging
 import typing
 
 if typing.TYPE_CHECKING:
-    from loguru import Logger
+    from loguru import Logger as LoguruLogger
+    Logger = typing.Union[LoguruLogger, logging.Logger]
 else:
-    Logger = None
+    Logger = typing.Any
 
 __all__ = ["logger"]
 
 
 def __get_logger() -> Logger:
-    from loguru import logger
+    try:
+        from loguru import logger as loguru_logger
 
-    return logger
+        return loguru_logger
+    except ModuleNotFoundError:
+        std_logger = logging.getLogger("vila_u")
+        if not std_logger.handlers:
+            handler = logging.StreamHandler()
+            handler.setFormatter(
+                logging.Formatter("[%(asctime)s] %(levelname)s %(name)s: %(message)s")
+            )
+            std_logger.addHandler(handler)
+        std_logger.setLevel(logging.INFO)
+        return std_logger
 
 
 logger = __get_logger()
